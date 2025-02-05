ec9d0077a172bb02068593ad0a83acabe992e5aa
timskillbridge <timskillbridge@gmail.com>
2025-02-05 11:01:40 -0600

    finished linear search and fibonacci

--- algo-linear-search-main/linearSearch.js
+++ algo-linear-search-main/linearSearch.js
@@ -1,9 +1,23 @@
 function linearSearch(searchTerm, arr) {
+
+  for(x = 0; x<arr.length; x++)
+    if (arr[x] == searchTerm) {
+      return x
+    }
   return undefined;
 }
 
 function globalLinearSearch(searchTerm, arr) {
-  return [];
+  answers = []
+  for(x=0; x<arr.length; x++)
+    if (arr[x] == searchTerm) {
+      answers.push(x)
+    }
+  if (answers.length == 0) {
+    return undefined
+  } else {return answers}
 }
 
+console.log(linearSearch("asdf",["a","b","c","asdf",false,true]))
+console.log(globalLinearSearch("mac",["steve","mac","mike","mac","Tony"]))
 module.exports = { linearSearch, globalLinearSearch };
