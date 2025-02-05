8f78db4efbcf453404cc4acc8a62c634774000ae
timskillbridge <timskillbridge@gmail.com>
2025-02-05 11:07:57 -0600

    done

--- algo-fibonacci-main/fibonacci.js
+++ algo-fibonacci-main/fibonacci.js
@@ -1,5 +1,16 @@
 function fibonacci(num) {
-  return 0;
-}
+  
+  if(num <= 2) {
+    return
+  }
+  runningTotal = [0,1]
+  for (x=2;x<num;x++) {
+    runningTotal.push(runningTotal[x-2]+runningTotal[x-1])
 
+  }
+  return runningTotal
+}
+console.log(fibonacci(5))
 module.exports = fibonacci;
+
+//done
